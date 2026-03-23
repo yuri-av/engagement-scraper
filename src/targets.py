9663a4058b1fc487226123ccda65f1867466c8e0
yuri-av <arancibiavieirayuri@gmail.com>
2026-03-23 17:36:45 -0300

    fase 1: definir_targets() implementada

---
+++ src/targets.py
@@ -0,0 +1,17 @@
+def definir_targets():
+    competidores = [
+        "https://www.instagram.com/mindcircus.agency/",
+        "https://www.instagram.com/agenciabuffalo/",
+    ]
+
+    temas = [
+        "IA en marketing",
+        "branding",
+        "diseño gráfico",
+        "SEO",
+    ]
+
+    return {
+        "competidores": competidores,
+        "temas": temas
+    }
